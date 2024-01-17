39898e4e56a26dd5011fcfa3c8ab4e899b00d6a3
Juanabely <abelkipkorir185@gmail.com>
2024-01-17 14:36:06 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
 /* Your Code Here */
+let createEmployeeRecord = function(recordDetail){
+    return {
+        firstName: recordDetail[0],
+        familyName: recordDetail[1],
+        title: recordDetail[2],
+        payPerHour: recordDetail[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(recordDetail){
+        return createEmployeeRecord(recordDetail)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
 /*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+We're giving you this function. Take a look at it, you might see some usage
+that's new and different. That's because we're avoiding a well-known, but
+sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+As a result, the lessons for this function will pass *and* it will be available
+for you to use if you need it!
+*/
 
+let hoursWorkedOnDate = function(exactDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === exactDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === exactDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateChosen){
+    let grossWage = hoursWorkedOnDate.call(this, dateChosen)
+        * this.payPerHour
+    return parseFloat(grossWage.toString())
+}
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +76,14 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function(orgArray, firstName) {
+    return orgArray.find(function(deets){
+    return deets.firstName === firstName
+    })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, deets){
+        return memo + allWagesFor.call(deets)
+    }, 0)
+}
